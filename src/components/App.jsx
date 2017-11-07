db7f0013342a1bcef1400a8c5de72ee1764cf192
aleksandrc-hyuna <aleksandr.chernokondratenko@playster.com>
2017-11-07 17:47:21 +0200

    add material-ui styles to auth component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
 
 const App = () => (
-  <Grid container spacing={40}>
-    <Grid item xs={6}>
-      <h2>Login</h2>
+  <Grid container alignItems="center" justify="center" direction="column" style={{ height: 'calc(100vh - 16px)' }}>
+    <Typography type="title" gutterBottom>
+      Login
+    </Typography>
+    <Grid item >
       <TextField
         placeholder="email"
       />
+    </Grid>
+    <Grid item>
       <TextField
         placeholder="password"
         type="password"
       />
+    </Grid>
+    <Grid item>
       <Button raised color="primary">
         Login
       </Button>
