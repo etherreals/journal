39a20b1d7e1f75634132ec39b285c8cec923b03c
Kapiroska <etherreals777@gmail.com>
2017-11-17 19:16:16 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import MainNav from './Nav/MainNav';
 
-const App = () => (
-  <MainNav />
-);
+const App = () => ([
+  <MainNav />,
+  <h1>app</h1>,
+]);
 
 export default App;
