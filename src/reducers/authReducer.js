c78ba3b1293f9edce6c9664298b9fba9e0b9cfac
Kapiroska <etherreals777@gmail.com>
2017-11-23 12:19:22 +0200

    refactored authReducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,51 +2,60 @@ import {
   AuthActionTypes,
 } from '../actions/types';
 
-const initialState = {};
+const handleLoginRequest = (state, action) => ({
+  ...state,
+  isLoading: action.payload.isLoading,
+  isLoggedIn: action.payload.isLoggedIn,
+  error: null,
+});
 
-function authReducer(state = initialState, action) {
-  switch (action.type) {
-    case AuthActionTypes.LOGIN_REQUEST:
-      return {
-        ...state,
-        isLoading: action.payload.isLoading,
-        isLoggedIn: action.payload.isLoggedIn,
-      };
-    case AuthActionTypes.LOGIN_SUCCESS:
-      return {
-        ...state,
-        isLoading: action.payload.isLoading,
-        isLoggedIn: action.payload.isLoggedIn,
-      };
-    case AuthActionTypes.LOGIN_FAILURE:
-      return {
-        ...state,
-        isLoading: action.payload.isLoading,
-        isLoggedIn: action.payload.isLoggedIn,
-        error: action.error,
-      };
-    case AuthActionTypes.LOGOUT_REQUEST:
-      return {
-        ...state,
-        isLoading: action.payload.isLoading,
-        isLoggedIn: action.payload.isLoggedIn,
-      };
-    case AuthActionTypes.LOGOUT_SUCCESS:
-      return {
-        ...state,
-        isLoading: action.payload.isLoading,
-        isLoggedIn: action.payload.isLoggedIn,
-      };
-    case AuthActionTypes.LOGOUT_FAILURE:
-      return {
-        ...state,
-        isLoading: action.payload.isLoading,
-        isLoggedIn: action.payload.isLoggedIn,
-        error: action.error,
-      };
-    default:
-      return state;
-  }
+const handleLoginSuccess = (state, action) => ({
+  ...state,
+  isLoading: action.payload.isLoading,
+  isLoggedIn: action.payload.isLoggedIn,
+  error: null,
+});
+
+const handleLoginFailure = (state, action) => ({
+  ...state,
+  isLoading: action.payload.isLoading,
+  isLoggedIn: action.payload.isLoggedIn,
+  error: action.error,
+});
+
+const handleLogoutRequest = (state, action) => ({
+  ...state,
+  isLoading: action.payload.isLoading,
+  isLoggedIn: action.payload.isLoggedIn,
+  error: null,
+});
+
+const handleLogoutSuccess = (state, action) => ({
+  ...state,
+  isLoading: action.payload.isLoading,
+  isLoggedIn: action.payload.isLoggedIn,
+  error: null,
+});
+
+const handleLogoutFailure = (state, action) => ({
+  ...state,
+  isLoading: action.payload.isLoading,
+  isLoggedIn: action.payload.isLoggedIn,
+  error: action.error,
+});
+
+const handlers = {
+  [AuthActionTypes.LOGIN_REQUEST]: handleLoginRequest,
+  [AuthActionTypes.LOGIN_SUCCESS]: handleLoginSuccess,
+  [AuthActionTypes.LOGIN_FAILURE]: handleLoginFailure,
+  [AuthActionTypes.LOGOUT_REQUEST]: handleLogoutRequest,
+  [AuthActionTypes.LOGOUT_SUCCESS]: handleLogoutSuccess,
+  [AuthActionTypes.LOGOUT_FAILURE]: handleLogoutFailure,
+};
+
+function authReducer(state = {}, action) {
+  const handler = handlers[action.type];
+  return handler ? handler(state, action) : state;
 }
 
 export default authReducer;
