115a306251fb5bf6c65240c0a0bdbc8500bc9e51
Kapiroska <etherreals777@gmail.com>
2017-11-30 17:07:54 +0200

    added pagination and sorting markup to pupils list

--- src/components/Pupils/PupilsList/PupilsList.jsx
+++ src/components/Pupils/PupilsList/PupilsList.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import keycode from 'keycode';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators, compose } from 'redux';
 import { withStyles } from 'material-ui/styles';
-import Table, { TableBody } from 'material-ui/Table';
+import Table, {
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+} from 'material-ui/Table';
 import Paper from 'material-ui/Paper';
 import * as actionCreators from '../../../actions/usersActions';
 import PupilItem from '../PupilItem/PupilItem';
@@ -11,45 +20,191 @@ import styles from './PupilsList.styles';
 import PupilsListHeader from './PupilsListHeader';
 import LoadingSpinner from '../../Common/LoadingSpinner';
 
+// class PupilsList extends Component {
+//   static propTypes = {
+//     actions: PropTypes.objectOf(PropTypes.func).isRequired,
+//     classes: PropTypes.objectOf(PropTypes.string).isRequired,
+//     pupils: PropTypes.arrayOf(PropTypes.object),
+//     isLoading: PropTypes.bool.isRequired,
+//   };
+//   static defaultProps = {
+//     pupils: [],
+//   };
+//   componentDidMount() {
+//     this.unsubscribe = this.props.actions.subscribeToGetAllPupilsListener();
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   render() {
+//     const { classes, pupils, isLoading } = this.props;
+//     return (
+//       <Paper className={classes.root}>
+//         <Table className={classes.table}>
+//           <PupilsListHeader />
+//           <TableBody>
+//             {
+//               pupils.map(pupil =>
+//                 (
+//                   <PupilItem
+//                     key={pupil.id}
+//                     id={pupil.id}
+//                     fullName={pupil.fullName}
+//                     dateOfBirth={pupil.dateOfBirth.toLocaleString()}
+//                     grade={pupil.grade}
+//                   />
+//                 ))
+//             }
+//           </TableBody>
+//         </Table>
+//         {isLoading && <LoadingSpinner />}
+//       </Paper>
+//     );
+//   }
+// }
+
+let counter = 0;
+function createData(name, calories, fat, carbs, protein) {
+  counter += 1;
+  return { id: counter, name, calories, fat, carbs, protein };
+}
 class PupilsList extends Component {
-  static propTypes = {
-    actions: PropTypes.objectOf(PropTypes.func).isRequired,
-    classes: PropTypes.objectOf(PropTypes.string).isRequired,
-    pupils: PropTypes.arrayOf(PropTypes.object),
-    isLoading: PropTypes.bool.isRequired,
-  };
-  static defaultProps = {
-    pupils: [],
-  };
-  componentDidMount() {
-    this.unsubscribe = this.props.actions.subscribeToGetAllPupilsListener();
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      order: 'asc',
+      orderBy: 'calories',
+      selected: [],
+      data: [
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Donut', 452, 25.0, 51, 4.9),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+        createData('Honeycomb', 408, 3.2, 87, 6.5),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Jelly Bean', 375, 0.0, 94, 0.0),
+        createData('KitKat', 518, 26.0, 65, 7.0),
+        createData('Lollipop', 392, 0.2, 98, 0.0),
+        createData('Marshmallow', 318, 0, 81, 2.0),
+        createData('Nougat', 360, 19.0, 9, 37.0),
+        createData('Oreo', 437, 18.0, 63, 4.0),
+      ].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
+      page: 0,
+      rowsPerPage: 5,
+    };
   }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    const data =
+      order === 'desc'
+        ? this.state.data.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
+        : this.state.data.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1));
+
+    this.setState({ data, order, orderBy });
+  };
+
+  handleSelectAllClick = (event, checked) => {
+    if (checked) {
+      this.setState({ selected: this.state.data.map(n => n.id) });
+      return;
+    }
+    this.setState({ selected: [] });
+  };
+
+  handleKeyDown = (event, id) => {
+    if (keycode(event) === 'space') {
+      this.handleClick(event, id);
+    }
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = (event) => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
 
   render() {
     const { classes, pupils, isLoading } = this.props;
+    const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
     return (
       <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <PupilsListHeader />
-          <TableBody>
-            {
-              pupils.map(pupil =>
-                (
-                  <PupilItem
-                    key={pupil.id}
-                    id={pupil.id}
-                    fullName={pupil.fullName}
-                    dateOfBirth={pupil.dateOfBirth.toLocaleString()}
-                    grade={pupil.grade}
-                  />
-                ))
-            }
-          </TableBody>
-        </Table>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table}>
+            <PupilsListHeader
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={this.handleSelectAllClick}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}
+            />
+            <TableBody>
+              {pupils.map(pupil => (
+                <PupilItem
+                  key={pupil.id}
+                  id={pupil.id}
+                  fullName={pupil.fullName}
+                  dateOfBirth={pupil.dateOfBirth.toLocaleString()}
+                  grade={pupil.grade}
+                />
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  count={data.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
         {isLoading && <LoadingSpinner />}
       </Paper>
     );
